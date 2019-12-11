278f23158de84558a789fba8b00737a9a49c4a5d
Cale Bierman <cale-bierman@uiowa.edu>
2019-12-10 20:21:32 -0600

    adding new ux code

---
+++ assets/js/offcanvas.js
@@ -0,0 +1,7 @@
+$(function () {
+  'use strict'
+
+  $('.sidebar-toggle').on('click', function () {
+    $('.sidebar').toggleClass('open')
+  })
+});
---
+++ assets/js/projects.js
@@ -0,0 +1,10 @@
+(function() {
+    $(document).ready( function () {
+        $('#projectsTable').DataTable(
+            {
+                responsive: true,
+                dom: "ftpli"
+            }
+        );
+    } );
+})();
